b50b83e09d138eb2d84b8a0ba424afcd60bb114f
Dejan Boras <de.jean.7777@protonmail.com>
2022-02-08 21:57:05 +0100

    Method to setup paths in storage.dart

--- lib/utils/storage.dart
+++ lib/utils/storage.dart
@@ -3,6 +3,14 @@ import 'dart:io';
 var documentsPath = '';
 var temporaryPath = '';
 
+Future<void> setupPaths() async {
+  documentsPath = await getDocumentsPath();
+  var dir = await getTemporaryDirectory();
+  temporaryPath = dir.path;
+  print('Documents path: ' + documentsPath);
+  print('Temporary files path: ' + temporaryPath);
+}
+
 /// delete the given file and prevent exceptions
 Future<bool> deleteFile(path) async {
   var ok = false;
