a4acbc40ed5fa76ba56ea3808fe8337cadb8e42b
Dejan Boras <de.jean.7777@protonmail.com>
2022-06-10 14:40:42 +0200

    Extension method to capitalize a string

--- lib/utils/string_utils.dart
+++ lib/utils/string_utils.dart
@@ -2,4 +2,8 @@ extension StringUtils on String {
   bool equalCaseInsensitive(String other) {
     return this.toLowerCase() == other.toLowerCase();
   }
+
+  String capitalize() {
+    return "${this[0].toUpperCase()}${this.substring(1).toLowerCase()}";
+  }
 }
