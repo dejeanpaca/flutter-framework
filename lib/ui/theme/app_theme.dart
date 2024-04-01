efd67723dcb15922d357804a8276a6934b434fb9
Dejan Boras <de.jean.7777@protonmail.com>
2024-04-01 15:22:08 +0200

    Section titles now use secondary text color

--- lib/ui/theme/app_theme.dart
+++ lib/ui/theme/app_theme.dart
@@ -5,13 +5,7 @@ class AppTheme {
   String name = '';
   late ThemeData theme;
 
-  AppTheme({bool isDark = false, String name = '', required ThemeData theme}) {
-    this.isDark = isDark;
-    this.name = name;
-    this.theme = theme;
-  }
+  AppTheme({this.isDark = false, this.name = '', required this.theme});
 
-  void construct() {
-
-  }
+  void construct() {}
 }
