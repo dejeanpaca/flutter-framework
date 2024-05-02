8cd850ec36a8c6f3e97d82e0508551dda6d4efec
Dejan Boras <de.jean.7777@protonmail.com>
2024-05-02 12:40:01 +0200

    setSystemOverlayStyle() van also set status bar brightness

--- lib/utils/system_theme.dart
+++ lib/utils/system_theme.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void setSystemOverlayStyle({Color? navbarColor, Color? statusBarcolor = Colors.transparent}) {
+void setSystemOverlayStyle(
+    {Color? navbarColor, Color? statusBarcolor = Colors.transparent, Brightness? statusBarBrightness}) {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     systemNavigationBarColor: navbarColor,
     statusBarColor: statusBarcolor,
+    statusBarBrightness: statusBarBrightness,
   ));
 }
