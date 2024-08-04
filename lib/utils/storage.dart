a7f0dd66d62f75d9258764f69f03372683b76031
Dejan Boras <de.jean.7777@protonmail.com>
2024-08-04 11:40:01 +0200

    setupPaths() now also gets the library path

--- lib/utils/storage.dart
+++ lib/utils/storage.dart
@@ -4,16 +4,21 @@ import 'dart:typed_data';
 import 'package:path_provider/path_provider.dart';
 
 var documentsPath = '';
+var libraryPath = '';
 var temporaryPath = '';
 
 Future<void> setupPaths() async {
   var dir = await getApplicationDocumentsDirectory();
   documentsPath = dir.path;
 
+  dir = await getLibraryDirectory();
+  libraryPath = dir.path;
+
   dir = await getTemporaryDirectory();
   temporaryPath = dir.path;
 
   print('Documents path: ' + documentsPath);
+  print('Library path: ' + libraryPath);
   print('Temporary files path: ' + temporaryPath);
 }
 
@@ -26,9 +31,7 @@ Future<bool> deleteFile(path) async {
   try {
     await f.delete();
     ok = true;
-  } catch (e) {
-
-  }
+  } catch (e) {}
 
   return ok;
 }
